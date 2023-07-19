a4219c39e094aedfc97f62a2d27802c081b6017e
Cevat <127509097+CevatEkici@users.noreply.github.com>
2023-07-19 19:13:05 +0300

    Add files via upload

---
+++ QR Code Oluşturucu/QRCoderConsole/DataObjects/SupportedImageFormat.cs
@@ -0,0 +1,17 @@
+﻿namespace QRCoderConsole.DataObjects
+{
+    public enum SupportedImageFormat
+    {
+        Png,
+        Jpg,
+        Gif,
+        Bmp,
+        Tiff,
+        Svg,
+#if !NET5_0 || NET5_0_WINDOWS
+        Xaml,
+#endif
+        Ps,
+        Eps,
+    }
+}
